9486f223a6593c192b08c35d3467e31fe5897b16
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-12 17:33:49 -0400

    Update drizzle schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 export default async function Page() {
   return (
     <div>
+      {/* should redirect to chat, which should route to a newChat */}
+      {/* new chat should be anonymously owned u*/}
     </div>
   )
 }
