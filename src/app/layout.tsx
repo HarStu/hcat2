04f4ae70df1aaf13ab46f34168c881bcfe3fadc6
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-12 20:12:52 -0400

    Implement list of extra games when you log in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,15 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner"
 import { cookies } from 'next/headers'
+import Link from "next/link";
+
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
 
 import { GoogleSignIn } from '@/components/google-signin'
 import { Logout } from '@/components/logout'
+
+import { gameConfigs } from '@/lib/games'
 
 import "./globals.css";
 
@@ -32,15 +38,36 @@ export default async function RootLayout({
   const sessionCookie = cookieStore.get('better-auth.session_token')
   console.log(`${sessionCookie}`)
 
+  const generateSidebar = () => {
+    if (sessionCookie) {
+      return (
+        gameConfigs.map((game) => {
+          return (
+            <Button key={game.name} variant="link" className="w-24 gap-4 m-4" asChild>
+              <Link href={`/play/${game.name}`}>{game.name}</Link>
+            </Button >
+          )
+        })
+      )
+    } else {
+      return (
+        <div>hello</div>
+      )
+    }
+  }
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex outline justify-end">
-          {sessionCookie ? <Logout /> : <GoogleSignIn />}
+        <div className="flex">
+          <div className="flex flex-col">
+            {sessionCookie ? <Logout /> : <GoogleSignIn />}
+            {generateSidebar()}
+          </div>
+          {children}
         </div>
-        {children}
         <Toaster />
       </body>
     </html>
